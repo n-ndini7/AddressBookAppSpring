1f2cbf3a98ac243063b45b35bd5bb1693a1b7f84
n-ndini7 <hp@RJs-PC>
2020-12-12 13:04:39 +0530

    [Nandini] Added UC3 - Introduced DTO and Model to address book app\

--- src/main/java/com/dto/AddressBookDTO.java
+++ src/main/java/com/dto/AddressBookDTO.java
@@ -2,4 +2,96 @@ package com.dto;
 
 public class AddressBookDTO {
 
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	private String phoneNo;
+	private String email;
+
+	public AddressBookDTO(String firstName, String lastName, String address, String state, String city, String zip,
+			String phoneNo, String email) {
+		this.setFirstName(firstName);
+		this.setLastName(lastName);
+		this.setAddress(address);
+		this.setState(state);
+		this.setCity(city);
+		this.setZip(zip);
+		this.setPhoneNo(phoneNo);
+		this.setEmail(email);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public void setPhoneNo(String phoneNo) {
+		this.phoneNo = phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "firstName : " + firstName + " ,lastName : " + lastName + " ,address : " + address + " ,state : " + state
+				+ " ,city : " + city + " ,zip : " + zip + " ,phoneNo : " + phoneNo + " ,email : " + email;
+	}
+
+	
 }
