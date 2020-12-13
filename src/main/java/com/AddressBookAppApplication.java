105f147d0d9319505ae35e56cab796a98f3bd242
n-ndini7 <hp@RJs-PC>
2020-12-13 14:05:36 +0530

    [Nandini] Added UC7 - added logging levels,files and patterns in properties file

--- src/main/java/com/AddressBookAppApplication.java
+++ src/main/java/com/AddressBookAppApplication.java
@@ -2,12 +2,17 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookAppApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(AddressBookAppApplication.class, args);
+		log.info("Address Book App Started");
+		
 	}
 
 }
